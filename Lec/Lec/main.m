888d9ee3bf3917c67c328dfd1fd52bb3eeaccf62
Matt <me_number3@hotmail.com>
2014-01-10 18:37:50 +1300

    Welcome to Lec! aka. LECORDIUM!

---
+++ Lec/Lec/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lec
+//
+//  Created by Matt Welson on 10/01/14.
+//  Copyright (c) 2014 South45. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LECAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LECAppDelegate class]));
+    }
+}
---
+++ Lec/LecTests/LecTests.m
@@ -0,0 +1,34 @@
+//
+//  LecTests.m
+//  LecTests
+//
+//  Created by Matt Welson on 10/01/14.
+//  Copyright (c) 2014 South45. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LecTests : XCTestCase
+
+@end
+
+@implementation LecTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
