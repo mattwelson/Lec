9bc4e0a4e18d3f45d1354e2a2579015bf9824316
Julin Le-Ngoc <julinlengoc@gmail.com>
2014-01-19 13:16:59 +1300

    Adding course add things.

--- Lec/Lec/ViewControllers/MainViewController.m
+++ Lec/Lec/ViewControllers/MainViewController.m
@@ -9,7 +9,9 @@
 #import "MainViewController.h"
 #import "LECImportHeader.h"
 
-@interface MainViewController ()
+@interface MainViewController (){
+    UIBarButtonItem *plusItem;
+}
 
 @end
 
@@ -52,10 +54,10 @@
     UINavigationItem *navItems = [[UINavigationItem alloc] init];
     
     UIImage *plusImg = [UIImage imageNamed:@"nav_add_btn.png"];
-    UIBarButtonItem *cameraItem = [[UIBarButtonItem alloc] initWithImage:plusImg style:UIBarButtonItemStylePlain target:self action:@selector(changeCoursePage)];
+    plusItem = [[UIBarButtonItem alloc] initWithImage:plusImg style:UIBarButtonItemStylePlain target:self action:@selector(addCourse)];
     
     navItems.title = @"Lec";
-    navItems.rightBarButtonItem = cameraItem;
+    navItems.rightBarButtonItem = plusItem;
     navBar.items = [NSArray arrayWithObject:navItems];
     [self.view addSubview:navBar];
 }
@@ -105,9 +107,40 @@
     [self.navigationController pushViewController:[[CourseViewController alloc] initWithNibName:@"CourseViewController" bundle:nil] animated:YES];
 }
 
-- (void) addCourseBut
+- (void)addCourse
 {
     //This is where we will add Courses to the tableView
+    UIView *addCourseView = [[UIView alloc]initWithFrame:CGRectMake(0, 60, self.view.frame.size.width, 0)];
+    addCourseView.backgroundColor = [UIColor whiteColor];
+    
+
+    [UIView animateWithDuration:0.2
+                          delay:0.0
+                        options: UIViewAnimationOptionCurveEaseIn
+                     animations:^{
+                         addCourseView.frame = CGRectMake(0, 60, self.view.frame.size.width, 100);
+                         self.courseView.frame = CGRectMake(0, 165, 320, self.view.frame.size.height-64);
+                     }
+                     completion:^(BOOL finished){
+                         UITextField *courseNameInput = [[UITextField alloc]initWithFrame:CGRectMake(60, 10, addCourseView.frame.size.width-60, addCourseView.frame.size.height/2)];
+                         courseNameInput.placeholder = @"Course Name";
+                         [courseNameInput setFont:[UIFont fontWithName:@"Avenir" size:30]];
+                         [addCourseView addSubview:courseNameInput];
+                         
+                         UITextField *courseDescriptorInput = [[UITextField alloc]initWithFrame:CGRectMake(60, addCourseView.frame.size.height/2, addCourseView.frame.size.width-60, addCourseView.frame.size.height/2)];
+                         courseDescriptorInput.placeholder = @"Course Description";
+                         [courseDescriptorInput setFont:[UIFont fontWithName:@"Avenir" size:15]];
+                         [addCourseView addSubview:courseDescriptorInput];
+                         
+                         UIImage *tickImg = [UIImage imageNamed:@"nav_add_btn.png"];
+                         plusItem = [[UIBarButtonItem alloc] initWithImage:tickImg style:UIBarButtonItemStylePlain target:self action:@selector(saveCourse)];
+                     }];
+    [self.view addSubview:addCourseView];
+}
+
+-(void)saveCourse{
+    UIImage *plusImg = [UIImage imageNamed:@"nav_add_btn.png"];
+    plusItem = [[UIBarButtonItem alloc] initWithImage:plusImg style:UIBarButtonItemStylePlain target:self action:@selector(addCourse)];
 }
 
 #pragma mark - DUMMY TESTS
