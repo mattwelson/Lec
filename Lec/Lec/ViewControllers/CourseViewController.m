f4b918d99bae661009e233888c374aff9d757549
Juni Lee <junisnow@live.com>
2014-02-19 02:55:16 +1300

    dismiss keyboard when tapped outside of keyboard. return key also dismisses keyboard. changes colour and adds new icon when selected when tick button is pressed saves requires a restart to see changes though!

--- Lec/Lec/ViewControllers/CourseViewController.m
+++ Lec/Lec/ViewControllers/CourseViewController.m
@@ -15,6 +15,11 @@
     Course *currentCourse;
     UIView *editView;
     NSArray *colourNames;
+    NSArray *iconNames;
+    UITextField *newCourseName;
+    UITextField *newDescription;
+    NSString *newColor;
+    NSString *newIcon;
 }
 
 @end
@@ -52,36 +57,50 @@
 
 -(void) courseEdit
 {
+    newIcon = currentCourse.icon;
     editView = [[UIView alloc] initWithFrame:self.view.bounds];
     [[LECColourService sharedColourService] addGradientForColour:[currentCourse colour] toView:editView];
-    //[[LECColourService sharedColourService] changeGradientToColour:@"Pink" forView:editView];
     
     UILabel *courseName = [[UILabel alloc] initWithFrame:CGRectMake(20, 50, 200, 50)];
     UILabel *descriptionName = [[UILabel alloc] initWithFrame:CGRectMake(20, 100, 200, 50)];
-    UITextField *newCourseName = [[UITextField alloc] initWithFrame:CGRectMake(130, 50, 200, 50)];
-    UITextField *newDescription = [[UITextField alloc] initWithFrame:CGRectMake(130, 100, 200, 50)];
     
-    UIPageControl *colorPage = [[UIPageControl alloc] initWithFrame:CGRectMake(0, 400, 320, 100)];
-    colorPage.numberOfPages = 3;
-    NSLog(@"%f, %f, %f, %f" , colorPage.bounds.origin.x, colorPage.bounds.origin.y, colorPage.bounds.size.width, colorPage.bounds.size.height);
-    [colorPage setBackgroundColor:[[UIColor blackColor] colorWithAlphaComponent:0.5f]];
+    UILabel *colorPicker = [[UILabel alloc] initWithFrame:CGRectMake(20, 150, 200, 50)];
+    UILabel *iconPicker = [[UILabel alloc] initWithFrame:CGRectMake(20, 300, 200, 50)];
+    
+    newCourseName = [[UITextField alloc] initWithFrame:CGRectMake(130, 50, 200, 50)];
+    newDescription = [[UITextField alloc] initWithFrame:CGRectMake(130, 100, 200, 50)];
+    
+    UIScrollView *colorView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 200, 320, 100)];
+    [colorView setContentSize:CGSizeMake(680, 100)];
+    
+    UIScrollView *iconView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 350, 320, 100)];
+    [iconView setContentSize:CGSizeMake(680, 100)];
+    
+    NSLog(@"%f, %f, %f, %f" , colorView.bounds.origin.x, colorView.bounds.origin.y, colorView.bounds.size.width, colorView.bounds.size.height);
+    [colorView setBackgroundColor:[[UIColor blackColor] colorWithAlphaComponent:0.5f]];
+    [iconView setBackgroundColor:[[UIColor blackColor] colorWithAlphaComponent:0.5f]];
     
     colourNames = [[LECColourService sharedColourService] colourKeys];
+    iconNames = [[LECIconService sharedIconService] iconKeys];
     NSMutableArray *cButtonArray = [NSMutableArray array];
     UIButton *tmp;
     CGSize size = CGSizeMake(45, 45);
-    CGFloat xOffset = 60;
-    CGFloat yOffset = 150;
+    CGFloat xOffset = 70;
+    CGFloat yOffset = 25;
     CGFloat lineSpace = 30;
     
     for (int i = 0; i < colourNames.count; i ++)
     {
         tmp = [UIButton buttonWithType:UIButtonTypeCustom];
         tmp.frame = CGRectMake(
-                               xOffset + ((i % 3) * (size.width + lineSpace)),
-                               yOffset + ((i / 3) * (size.height + lineSpace)),
+                               i*xOffset+lineSpace,
+                               yOffset,
                                size.width,
                                size.height);
+        NSLog(@"%f, %f, %f, %f" , xOffset * (size.width + lineSpace),
+              yOffset * (size.height + lineSpace),
+              size.width,
+              size.height);
         [[LECColourService sharedColourService] addGradientForColour:colourNames[i] toView:tmp];
         tmp.layer.cornerRadius = tmp.frame.size.height/2;
         tmp.layer.masksToBounds = YES;
@@ -90,27 +109,76 @@
         tmp.tag = i;
         [tmp addTarget:self action:@selector(colourSelected:) forControlEvents:UIControlEventTouchUpInside];
         
-        [editView addSubview:tmp];
+        [colorView addSubview:tmp];
     }
+    
+    for (int i = 0; i < iconNames.count; i ++)
+    {
+        tmp = [UIButton buttonWithType:UIButtonTypeCustom];
+        tmp.frame = CGRectMake(
+                               i*xOffset+lineSpace-20,
+                               yOffset-25,
+                               size.width,
+                               size.height);
+        NSLog(@"%f, %f, %f, %f" , xOffset * (size.width + lineSpace),
+              yOffset * (size.height + lineSpace),
+              size.width,
+              size.height);
+        [[LECIconService sharedIconService] addIcon:iconNames[i] toView:tmp];
+        [cButtonArray addObject:tmp];
+        tmp.tag = i;
+        [tmp addTarget:self action:@selector(iconSelected:) forControlEvents:UIControlEventTouchUpInside];
+        
+        [iconView addSubview:tmp];
+    }
+    
+    
     
     [courseName setTextColor:[UIColor whiteColor]];
     [descriptionName setTextColor:[UIColor whiteColor]];
     [newCourseName setTextColor:[UIColor whiteColor]];
     [newDescription setTextColor:[UIColor whiteColor]];
+    [colorPicker setTextColor:[UIColor whiteColor]];
+    [iconPicker setTextColor:[UIColor whiteColor]];
+    [colorPicker setText:@"Course Colour: "];
+    [iconPicker setText:@"Course Icon: "];
     [courseName setText:@"Course: "];
     [descriptionName setText:@"Description: "];
     [newCourseName setText:viewModel.navTitle];
     [newDescription setText:viewModel.subTitle];
     
-    self.navigationItem.rightBarButtonItem = nil;
+    UIImage *checkImg = [UIImage imageNamed:@"icon_checkmark.png"];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithImage:checkImg style:UIBarButtonItemStylePlain target:self action:@selector(saveEdit)];
     self.navigationItem.title = nil;
     
+    [editView addSubview:iconPicker];
+    [editView addSubview:colorPicker];
     [editView addSubview:courseName];
     [editView addSubview:descriptionName];
     [editView addSubview:newCourseName];
     [editView addSubview:newDescription];
-    [editView addSubview:colorPage];
+    [editView addSubview:colorView];
+    [editView addSubview:iconView];
     [self.view addSubview:editView];
+}
+
+-(void) saveEdit
+{
+    currentCourse.courseName = newCourseName.text;
+    currentCourse.courseDescription = newDescription.text;
+    currentCourse.colour = newColor;
+    currentCourse.icon = newIcon;
+    [[LECDatabaseService sharedDBService] saveChanges];
+    [editView removeFromSuperview];
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (IBAction)iconSelected:(id)sender
+{
+    NSInteger i = ((UIButton *)sender).tag;
+    NSLog(@"%@",iconNames[i]);
+    newIcon = iconNames[i];
+    
 }
 
 - (IBAction)colourSelected:(id)sender
@@ -118,6 +186,8 @@
     NSInteger i = ((UIButton *)sender).tag;
     NSLog(@"%@",colourNames[i]);
     [[LECColourService sharedColourService] changeGradientToColour:colourNames[i] forView:editView];
+    newColor = colourNames[i];
+    
     
 }
 
@@ -181,6 +251,18 @@
 -(NSInteger) numberOfSections
 {
     return 3;
+}
+
+// Dismissing the Keyboard when touch event is called by touching screen
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    NSLog(@"Keyboard is being dismissed");
+    [self.view endEditing:YES];
+    [super touchesBegan:touches withEvent:event];
+}
+
+- (BOOL) textFieldShouldReturn:(UITextField *)textField
+{
+    return [self.view endEditing:YES];
 }
 
 @end
